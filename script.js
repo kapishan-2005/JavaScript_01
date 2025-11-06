506ffd7d26d198beed3f375a8681107079d23443
kapishan-2005 <kapishan.official@gmail.com>
2025-11-06 12:33:35 +0530

    first java

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello");
+console.log("kapishan");
+console.log("bike ride");
+console.log("50+20");
+document.getElementById("text").innerHTML="Hello Uki";
+document.write("Welcome to javascript");
+// window.alert("wrong");
+console.log("javascript is fun");
+var subject="maths"
+let name="kapishan"
+const age="20"
+console.log(name,subject,age);
+var favourite= prompt("input favourite color");
+console.log(favourite);
+document.body.style.backgroundColor=favourite;
+var price=5;
+var quantity= 10;
+var display= price*quantity;
+console.log(display);
